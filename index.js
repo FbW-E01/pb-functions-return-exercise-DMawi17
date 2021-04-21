0f93697152432b9f4a2446f6f5bd842ced3e9b42
DMawi17 <asegda17@gmail.com>
2021-04-21 16:07:23 +0200

    return exercise

--- index.js
+++ index.js
@@ -2,18 +2,20 @@
 
 // Otherwise returns false:
 
-
-function checkAge(age) {
-
-  }
-
+checkAge = (age) => {
+    if (age >= 18) {
+        isItTrue = true;
+    } else {
+        isItTrue = false;
+    }
+    console.log(isItTrue);
+};
+checkAge(18);
 
 // Write a function min(a,b) which returns the least of two numbers a and b.
-  function min(a, b) {
-   
-  }
+min = (a, b) => console.log(Math.min(a, b));
+min(5, 3);
 
 // Write a function pow(x,n) that returns x in power n. Or, in other words, multiplies x by itself n times and returns the result.
-  function pow(x, n) {
-    
-  }
+pow = (x, n) => console.log((isItSimple = x ** n));
+pow(3, 3);
